2b2ef3ba59b90cd99bf8efada92b43a0f276461a
Jerry Feng <jerry.zj.feng@gmail.com>
2017-06-27 16:47:26 -0400

    membership and profile initial models

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'SelfStorage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SelfStorage',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
---
+++ api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Membership(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, default='')
+    monthly_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+
+class Profile(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, blank=True, default='')
+    membership = models.ForeignKey(Membership, on_delete=models.CASCADE)
